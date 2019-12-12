ce08cfd7b6255726e8dde27201c55b858550f7c4
EkaterinaP1 <55790213+EkaterinaP1@users.noreply.github.com>
2019-12-12 15:59:33 +1000

    Add files via upload

---
+++ Указатели.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+void func( int pointerArr[5])
+{
+	for (int i = 0; i < 5; i++)
+
+	{
+		cout << pointerArr[i];
+	}
+}
+
+void func2(int* pa) {
+	(*pa) = 4;
+}
+
+
+
+int main(int* pa)
+{
+	int arr[5];
+	int* pointerArr = arr;
+	int a = 1;
+
+	func2(&a);
+
+	for (int i = 0; i < 5; i++)
+    {
+      arr[i] = a;
+    }
+	
+	func(pointerArr);
+}
